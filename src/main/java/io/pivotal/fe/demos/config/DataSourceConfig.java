372d65e2bfa6ca89dfee910159e6cc561a0b9f77
Sufyaan Kazi <sufyaan_kazi@hotmail.com>
2017-07-14 12:38:33 +0100

    Added new code

--- src/main/java/io/pivotal/fe/demos/config/DataSourceConfig.java
+++ src/main/java/io/pivotal/fe/demos/config/DataSourceConfig.java
@@ -1,68 +1,101 @@
 package io.pivotal.fe.demos.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.DriverManager;
 
 @Configuration
-@ConfigurationProperties(prefix="spring.datasource")
+//@ConfigurationProperties(prefix="spring.datasource")
 @Profile("local")
 public class DataSourceConfig {
-//	private String driverClassName;
-//	private String url;
-//	private String username;
-//	private String password;
-//
-//	public DataSourceConfig() {
-//		super();
-//	}
-//
-//	//@Bean
-//	public DataSource dataSource() {
-//		SimpleDriverDataSource dataSource = null;
-//		try {
-//			Class.forName(this.driverClassName);
-//			dataSource = new SimpleDriverDataSource();
-//			dataSource.setDriver(DriverManager.getDriver(this.url));
-//			dataSource.setUrl(this.url);
-//			dataSource.setUsername(this.username);
-//			dataSource.setPassword(this.password);
-//		} catch (Exception e) {
-//			throw new IllegalStateException("An Exception occurred initialising datasource", e);
-//		}
-//
-//		return dataSource;
-//	}
-//
-//	public String getDriverClassName() {
-//		return driverClassName;
-//	}
-//
-//	public void setDriverClassName(String driverClassName) {
-//		this.driverClassName = driverClassName;
-//	}
-//
-//	public String getUrl() {
-//		return url;
-//	}
-//
-//	public void setUrl(String url) {
-//		this.url = url;
-//	}
-//
-//	public String getUsername() {
-//		return username;
-//	}
-//
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-//
-//	public String getPassword() {
-//		return password;
-//	}
-//
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
+
+    //private String driverClassName;
+    @Value("${DB_PORT}")
+    private String port;
+    @Value("${DB_HOST}")
+    private String host;
+    @Value("${DB_NAME}")
+    private String dbName;
+    @Value("${DB_USER}")
+    private String username;
+    @Value("${DB_PASSWORD}")
+    private String password;
+
+    public DataSourceConfig() {
+        super();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = null;
+        try {
+            //Class.forName(this.driverClassName);
+            dataSource = new SimpleDriverDataSource();
+            StringBuilder url = new StringBuilder("jdbc:mysql://");
+            url.append(host).append(":").append(port).append("/").append(dbName);
+            dataSource.setDriver(DriverManager.getDriver(url.toString()));
+            dataSource.setUrl(url.toString());
+            dataSource.setUsername(this.username);
+            dataSource.setPassword(this.password);
+        } catch (Exception e) {
+            throw new IllegalStateException("An Exception occurred initialising datasource", e);
+        }
+
+        return dataSource;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+	public String getDriverClassName() {
+		//return driverClassName;
+        return "";
+	}
+
+	public void setDriverClassName(String driverClassName) {
+		//this.driverClassName = driverClassName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
