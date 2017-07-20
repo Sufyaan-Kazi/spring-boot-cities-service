77a34a0ceab412b3540c11d00951045b6cd56ce0
Sufyaan Kazi <sufyaan_kazi@hotmail.com>
2017-07-20 22:08:45 +0100

    combined jobs

--- ci/scripts/createProject.sh
+++ ci/scripts/createProject.sh
@@ -26,7 +26,8 @@ main()
     else
       PLAN=mysql-ephemeral
     fi
-    oc process openshift/$PLAN --param-file=src/main/resources/mysql.env -l name=${APPNAME} | oc create -f -
+    echo $PLAN
+    oc process openshift//$PLAN --param-file=src/main/resources/mysql.env -l name=${APPNAME} | oc create -f -
     sleep 3
     oc get logs dc/mysql
   fi
