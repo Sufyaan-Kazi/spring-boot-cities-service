f699395fab737d9151317223ee526d3605e04635
Sufyaan Kazi <sufyaan_kazi@hotmail.com>
2017-07-20 20:08:35 +0100

    corrections

--- ci/scripts/createProject.sh
+++ ci/scripts/createProject.sh
@@ -8,6 +8,7 @@ main()
     echo_msg "Creating Project"
     oc_login
     EXISTS=`oc projects | grep $APPNAME | wc -l | xargs`
+    echo $EXISTS
     if [ $EXISTS -ne 0 ]
     then
       oc delete project $APPNAME
