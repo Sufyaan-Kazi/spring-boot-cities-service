42d522ab556d27cf51105ca8daf8b6622755bf8f
Sufyaan Kazi <sufyaankazi@googlemail.com>
2018-03-06 23:03:23 +0000

    Updates

--- gcp_automation/depManager/smart/cleanup.sh
+++ gcp_automation/depManager/smart/cleanup.sh
@@ -3,12 +3,10 @@
 
 echo "********* Performing Cleanup *****"
 
-#gcloud deployment-manager deployments delete cities-service -q --async
-gcloud deployment-manager deployments delete cities-instances -q
-gcloud compute instances list
+gcloud deployment-manager deployments delete cities-instance-group -q
+gcloud deployment-manager deployments delete cities-instance-template -q
 
 gcloud deployment-manager deployments delete cities-firewall -q
-gcloud compute firewall-rules list --filter network=custom-network
 
 gsutil rm -raf gs://${BUCKET_NAME}/*
 gsutil rb -f gs://${BUCKET_NAME}/
--- gcp_automation/depManager/smart/depManage.sh
+++ gcp_automation/depManager/smart/depManage.sh
@@ -11,10 +11,25 @@ gsutil mb gs://${BUCKET_NAME}/
 gsutil cp -r startup-scripts/* gs://${BUCKET_NAME}/startup-scripts/
 gsutil ls -al gs://${BUCKET_NAME}/
 
-echo "Creating Instances"
+echo "Creating Instance Templates"
 gcloud deployment-manager deployments create cities-instance-templates --config instance-template.yml
+
+echo "Creating Instance Groups"
+gcloud deployment-manager deployments create cities-instance-group --config instance-group.yml
+gcloud compute instance-groups managed list-instances cities-service-ig --region=${SERVICE_REGION}
+gcloud compute instance-groups managed list-instances cities-service-ig --region=${SERVICE_REGION}
+INST=`gcloud compute instance-groups managed list-instances cities-service-ig --region=${SERVICE_REGION} | tail -n 1 | cut -d ' ' -f1 | xargs`
+SERVICE_ZONE=`gcloud compute instance-groups managed list-instances cities-service-ig --region=${SERVICE_REGION} | tail -n 1 | xargs | cut -d ' ' -f2 | xargs`
+echo $INST $SERVICE_ZONE
 echo "Sleeping while instance initialises"
-sleep 120
+sleep 60
+gcloud compute instances get-serial-port-output cities-service --zone=${SERVICE_ZONE}
+
+# Define Autoscaling
+echo "Autoscale"
+cat autoscale.yml | sed s/REGION/europe-west2/g > autoscale_temp.yml
+gcloud deployment-manager deployments create cities-service-as --config=autoscale_temp.yml
+rm -f autoscale_temp.yml
 
 echo "Creating Firewall Rules"
 gcloud deployment-manager deployments create cities-firewall --config firewall-rules.yml
