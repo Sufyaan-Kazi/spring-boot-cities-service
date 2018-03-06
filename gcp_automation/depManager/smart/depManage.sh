5b3ba8fcdbdc18bbc9097a7cd6307984557b4cae
Sufyaan Kazi <sufyaankazi@googlemail.com>
2018-03-06 22:04:11 +0000

    Updates

--- gcp_automation/depManager/smart/depManage.sh
+++ gcp_automation/depManager/smart/depManage.sh
@@ -12,18 +12,9 @@ gsutil cp -r startup-scripts/* gs://${BUCKET_NAME}/startup-scripts/
 gsutil ls -al gs://${BUCKET_NAME}/
 
 echo "Creating Instances"
-gcloud deployment-manager deployments create cities-instances-templates --config instance-templates.yml
-
-
-
-
-gcloud deployment-manager deployments create cities-instances --config instances.yml
-#gcloud compute instances tail-serial-port-output cities-service
-echo "Sleeping while instance initialises, monitor output using console or: gcloud compute instances tail-serial-port-output cities-service"
+gcloud deployment-manager deployments create cities-instance-templates --config instance-template.yml
+echo "Sleeping while instance initialises"
 sleep 120
-gcloud compute instances get-serial-port-output cities-service --zone=${SERVICE_ZONE}
-sleep 5
-gcloud compute instances get-serial-port-output cities-ui --zone=${UI_ZONE}
 
 echo "Creating Firewall Rules"
 gcloud deployment-manager deployments create cities-firewall --config firewall-rules.yml
