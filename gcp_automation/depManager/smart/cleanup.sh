6c9e3a680507e0a6ecf3a86059526542c2cd7380
Sufyaan Kazi <sufyaankazi@googlemail.com>
2018-03-07 00:11:06 +0000

    Got Instance groups, hc, autosclaing working

--- gcp_automation/depManager/smart/cleanup.sh
+++ gcp_automation/depManager/smart/cleanup.sh
@@ -2,11 +2,10 @@
 . vars.txt
 
 echo "********* Performing Cleanup *****"
-
-gcloud deployment-manager deployments delete cities-instance-group -q
-gcloud deployment-manager deployments delete cities-instance-template -q
-
-gcloud deployment-manager deployments delete cities-firewall -q
+gcloud deployment-manager deployments list
+gcloud deployment-manager deployments delete -q cities-service-as \
+    cities-service-ig cities-service-it cities-service-hc cities-firewall
+gcloud deployment-manager deployments list
 
 gsutil rm -raf gs://${BUCKET_NAME}/*
 gsutil rb -f gs://${BUCKET_NAME}/
--- gcp_automation/depManager/smart/depManage.sh
+++ gcp_automation/depManager/smart/depManage.sh
@@ -12,24 +12,28 @@ gsutil cp -r startup-scripts/* gs://${BUCKET_NAME}/startup-scripts/
 gsutil ls -al gs://${BUCKET_NAME}/
 
 echo "Creating Instance Templates"
-gcloud deployment-manager deployments create cities-instance-templates --config instance-template.yml
+gcloud deployment-manager deployments create cities-service-it --config instance-template.yml
 
 echo "Creating Instance Groups"
-gcloud deployment-manager deployments create cities-instance-group --config instance-group.yml
+gcloud deployment-manager deployments create cities-service-ig --config instance-group.yml
 gcloud compute instance-groups managed list-instances cities-service-ig --region=${SERVICE_REGION}
 gcloud compute instance-groups managed list-instances cities-service-ig --region=${SERVICE_REGION}
 INST=`gcloud compute instance-groups managed list-instances cities-service-ig --region=${SERVICE_REGION} | tail -n 1 | cut -d ' ' -f1 | xargs`
 SERVICE_ZONE=`gcloud compute instance-groups managed list-instances cities-service-ig --region=${SERVICE_REGION} | tail -n 1 | xargs | cut -d ' ' -f2 | xargs`
 echo $INST $SERVICE_ZONE
 echo "Sleeping while instance initialises"
-sleep 60
-gcloud compute instances get-serial-port-output cities-service --zone=${SERVICE_ZONE}
+sleep 120
+gcloud compute instances get-serial-port-output ${INST} --zone=${SERVICE_ZONE}
 
 # Define Autoscaling
-echo "Autoscale"
+echo "Setting up Autoscale"
 cat autoscale.yml | sed s/REGION/europe-west2/g > autoscale_temp.yml
 gcloud deployment-manager deployments create cities-service-as --config=autoscale_temp.yml
 rm -f autoscale_temp.yml
+
+echo "Linking HealthCheck to the Instance Group"
+gcloud deployment-manager deployments create cities-service-hc --config=healthcheck.yml
+gcloud beta compute instance-groups managed set-autohealing cities-service-ig --http-health-check=cities-service-hc --initial-delay=90 --region=europe-west2
 
 echo "Creating Firewall Rules"
 gcloud deployment-manager deployments create cities-firewall --config firewall-rules.yml
