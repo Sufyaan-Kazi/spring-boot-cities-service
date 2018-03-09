80f1c5a4463541e122ef05b1bd8f7c889686d337
Sufyaan Kazi <sufyaankazi@googlemail.com>
2018-03-09 16:18:11 +0000

    Simplified cleanup

--- gcp_automation/depManager/smart/cleanup.sh
+++ gcp_automation/depManager/smart/cleanup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash 
 . vars.txt
 
+deleteDeployment() {
+  EXIST=`echo $DEPS | grep $1 | wc -l`
+
+  if [ $EXIST -ne 0 ]
+  then
+    echo "Deleting Deployment: $1"
+    gcloud deployment-manager deployments delete -q $1
+  fi
+}
+
 echo "********* Performing Cleanup if necessary *****"
 
 #Temp Hack because can't used deployment manager for forwarding rules for some reason
@@ -8,21 +18,23 @@ echo "********* Performing Cleanup if necessary *****"
 COUNT=`gcloud deployment-manager deployments list | grep cities-service | wc -l`
 if [ $COUNT -ne 0 ]
 then
-  gcloud deployment-manager deployments list
-  gcloud deployment-manager deployments delete -q cities-service-fwd-rule
-  gcloud deployment-manager deployments delete -q cities-service-int-lb
-  gcloud deployment-manager deployments delete -q cities-service-as
-  gcloud deployment-manager deployments delete -q cities-service-ig
-  gcloud deployment-manager deployments delete -q cities-service-hc
-  gcloud deployment-manager deployments delete -q cities-service-it
-  gcloud deployment-manager deployments delete -q cities-instances
-  gcloud deployment-manager deployments delete -q cities-firewall
+  DEPS=`gcloud deployment-manager deployments list`
+
+  deleteDeployment cities-service-fwd-rule
+  deleteDeployment cities-service-int-lb
+  deleteDeployment cities-service-as
+  deleteDeployment cities-service-ig
+  deleteDeployment cities-service-hc
+  deleteDeployment cities-service-it
+  deleteDeployment cities-instances
+  deleteDeployment cities-firewall
   gcloud deployment-manager deployments list
 fi
 
 COUNT=`gsutil ls | grep ${BUCKET_NAME} | wc -l`
 if [ $COUNT -ne 0 ]
 then
+  echo "Deleting bucket"
   gsutil rm -raf gs://${BUCKET_NAME}/*
   gsutil rb -f gs://${BUCKET_NAME}/
 fi
