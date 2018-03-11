e21fcf676c928a57bb1ec87d98e31ff286dd95db
Sufyaan Kazi <sufyaankazi@googlemail.com>
2018-03-11 23:18:16 +0000

    Tidied up

--- gcp_automation/depManager/smart/cleanup.sh
+++ gcp_automation/depManager/smart/cleanup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash 
 . vars.txt
 
+deleteBucket() {
+  COUNT=`gsutil ls | grep ${BUCKET_NAME} | wc -l`
+  if [ $COUNT -ne 0 ]
+  then
+    echo "Deleting bucket"
+    gsutil rm -raf gs://${BUCKET_NAME}/*
+    gsutil rb -f gs://${BUCKET_NAME}/
+  fi
+}
+
 deleteDeployment() {
   EXIST=`echo $DEPS | grep $1 | wc -l`
 
@@ -11,32 +21,36 @@ deleteDeployment() {
   fi
 }
 
-echo "********* Performing Cleanup if necessary *****"
+deleteDeploymentAsync() {
+  EXIST=`echo $DEPS | grep $1 | wc -l`
 
-#Temp Hack because can't used deployment manager for forwarding rules for some reason
+  if [ $EXIST -ne 0 ]
+  then
+    echo "Deleting Deployment: $1"
+    gcloud deployment-manager deployments delete -q $1 --async
+  fi
+}
 
-COUNT=`gcloud deployment-manager deployments list | grep cities-service | wc -l`
-if [ $COUNT -ne 0 ]
-then
-  DEPS=`gcloud deployment-manager deployments list`
+deleteCitiesService() {
+  echo "********* Performing Cleanup if necessary *****"
+  COUNT=`gcloud deployment-manager deployments list | grep cities-service | wc -l`
+  if [ $COUNT -ne 0 ]
+  then
+    DEPS=`gcloud deployment-manager deployments list`
 
-  deleteDeployment cities-service-ig-fwd-rule
-  deleteDeployment cities-service-int-lb
-  deleteDeployment cities-service-ig-as
-  deleteDeployment cities-service-ig
-  deleteDeployment cities-service-ig-hc
-  deleteDeployment cities-service-it
-  deleteDeployment cities-instances
-  deleteDeployment cities-firewall
-  gcloud deployment-manager deployments list
-fi
+    deleteDeploymentAsync cities-firewall
+    deleteDeploymentAsync cities-instances
+    deleteDeployment cities-service-ig-fwd-rule
+    deleteDeployment cities-service-int-lb
+    deleteDeployment cities-service-ig-as
+    deleteDeployment cities-service-ig
+    deleteDeploymentAsync cities-service-ig-hc
+    deleteDeployment cities-service-it
+    gcloud deployment-manager deployments list
+  fi
+}
 
-COUNT=`gsutil ls | grep ${BUCKET_NAME} | wc -l`
-if [ $COUNT -ne 0 ]
-then
-  echo "Deleting bucket"
-  gsutil rm -raf gs://${BUCKET_NAME}/*
-  gsutil rb -f gs://${BUCKET_NAME}/
-fi
+deleteCitiesService
+deleteBucket
 
 echo "********* Cleanup Complete *****"
